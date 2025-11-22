c5dedae20b57846615017e31067e42b861eb6881
vigneshramadas <119106894+vigneshramadas@users.noreply.github.com>
2025-11-22 21:06:54 +0530

    updated

--- main.tf
+++ main.tf
@@ -4,8 +4,7 @@ provider "aws" {
 resource "aws_instance" "myec2" {
     ami = "ami-0fa3fe0fa7920f68e"
     instance_type = "t2.micro"
-tags =
-{
-    Name = "Github-Ec2"
+tags = {
+  Name = "Github-Ec2"
 }
 }
